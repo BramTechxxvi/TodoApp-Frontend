50c4de3660cdd9d1d833007b07ad08d87f5cbb41
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-27 21:42:42 +0100

    userapi

--- js/userApi.js
+++ js/userApi.js
@@ -3,21 +3,21 @@ async function loginUser(email, password) {
 }
 
 async function registerUser(firstName, lastName, email, password) {
-    return apiRequest("/users/register", "POST", { firstName, lastName, email, password });
+    return apiRequest("/users/register", "POST", { firstName, lastName, email, password }, false);
 }
 
 async function getProfile() {
-    return apiRequest("/users/profile", "Get");
+    return apiRequest("/users/profile", "Get", true);
 }
 
 async function changeEmail(oldEmail, newEmail) {
-    return apiRequest("/users/changeEmail/{id}", "PUT", { oldEmail, newEmail })
+    return apiRequest("/users/changeEmail/{id}", "PUT", { oldEmail, newEmail }, true)
 }
 
 async function changePassword(oldPassword, newPassword) {
-    return apiRequest("users/changePassword/{id}", "PUT", { oldPassword, newPassword })
+    return apiRequest("users/changePassword/{id}", "PUT", { oldPassword, newPassword }, true)
 }
 
 async function logout() {
-    return apiRequest("/users/logout/{id}", "POST")
+    return apiRequest("/users/logout/{id}", "POST", true)
 }
