dddd19ab694a22cbf8b1ff9427f32ca8289fd742
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-27 21:03:19 +0100

    api

--- js/api.js
+++ js/api.js
@@ -4,4 +4,19 @@ const API_BASE = "http://localhost:8080";
     return localStorage.getItem("token");
  };
 
- 
+ async function apiRequest(params) { 
+    const headers = { "Content-type": "application/json" };
+    if (isAuth && getAuthToken()) headers["Authorization"] = `Bearer ${getAuthToken()}`;
+
+    const response = await fetch(`${API_BASE}${endpoint}`, {
+        method,
+        headers,
+        body: data ? JSON.stringify(data) : null,
+    })
+
+    if(!response.ok) {
+        const error = await response.text();
+        throw new Error(error || "API error");
+    }
+    return response.json();;
+ }
